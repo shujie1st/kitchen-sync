e071686dc655ddf79ab45fbf905ba6089c2c6d53
eliachow <eliacchow@gmail.com>
2023-07-26 11:42:16 -0700

    create routes directory

---
+++ backend/routes/index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router  = express.Router();
+
+router.get('/ingredients', (req, res) => {
+})
+
+router.get('/preferences', (req, res) => {
+})
+
+
+
+module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -11,6 +11,22 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+
+
+// Separated Routes for each Resource
+const indexRoutes = require('./routes/index');
+const loginRoutes = require('./routes/login');
+const logoutRoutes = require('./routes/logout');
+const registerRoutes = require('./routes/register');
+
+// Mount all resource routes
+app.use('/', indexRoutes);
+app.use('/login', loginRoutes);
+app.use('/logout', logoutRoutes);
+app.use('/register', registerRoutes);
+
+
+
 app.get('/', (req, res) => res.send('Server running'));
 
 app.listen(PORT, () => {
