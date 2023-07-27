ac0213e31c5a893199cbc221f12004ebcb9b370c
Shujie Ma <shujie1st@gmail.com>
2023-07-27 01:02:52 -0400

    Setup API call & add search bar

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,7 +1,38 @@
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+
 function Recipe(){
+  // "https://api.edamam.com/api/recipes/v2?type=public&q=beef%20almond&app_id= &app_key= "
+  const api = process.env.REACT_APP_API;
+  const apiId = process.env.REACT_APP_API_ID;
+  const apiKeys = process.env.REACT_APP_API_KEYS;
+
+  let keywords = "shrimp lettuce";
+  let url = `${api}&q=${keywords}&app_id=${apiId}&app_key=${apiKeys}`;
+  let searchRecipes = function () {
+    fetch(url)
+      .then(response => response.json())
+      .then(data => console.log(data))
+  };
+
   return ( 
       <section className="recipes">
-       Recipe component
+        <Container className="search">
+      <Row>
+        <Col sm={4}>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Search"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button onClick={searchRecipes}>
+              Search
+            </Button>
+          </Form>
+        </Col>
+      </Row>
+    </Container>
       </section>
   );
 }
