163e5cd7be1b04fba2b4179e976db24e625feeaa
Shujie Ma <shujie1st@gmail.com>
2023-07-27 17:23:43 -0400

    Implement searching recipes by keywords using API

--- client/src/components/Recipe.js
+++ client/src/components/Recipe.js
@@ -1,30 +1,34 @@
+import { useRef } from "react";
 import { Button, Col, Container, Form, Row } from "react-bootstrap";
 
 function Recipe(){
-  // "https://api.edamam.com/api/recipes/v2?type=public&q=beef%20almond&app_id= &app_key= "
   const api = process.env.REACT_APP_API;
   const apiId = process.env.REACT_APP_API_ID;
   const apiKeys = process.env.REACT_APP_API_KEYS;
 
-  let keywords = "shrimp lettuce";
-  let url = `${api}&q=${keywords}&app_id=${apiId}&app_key=${apiKeys}`;
-  let searchRecipes = function () {
+  const inputElement = useRef();
+ 
+  const searchRecipes = () => {
+    const keywords = inputElement.current.value;
+    const url = `${api}&q=${keywords}&app_id=${apiId}&app_key=${apiKeys}`;
+
     fetch(url)
       .then(response => response.json())
       .then(data => console.log(data))
   };
 
   return ( 
-      <section className="recipes">
+      <section className="mt-5 recipes">
         <Container className="search">
       <Row>
         <Col sm={4}>
           <Form className="d-flex">
             <Form.Control
               type="search"
-              placeholder="Search"
+              placeholder="Search recipes"
               className="me-2"
               aria-label="Search"
+              ref={inputElement}
             />
             <Button onClick={searchRecipes}>
               Search
