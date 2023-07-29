5e361807f66c3f81a4a77cf997e9857bba37632b
eliachow <eliacchow@gmail.com>
2023-07-29 11:20:15 -0700

    refactor ingredients list to ingredients button

--- client/src/components/Ingredient.js
+++ client/src/components/Ingredient.js
@@ -9,7 +9,6 @@ const Ingredient = () => {
     try {
       const response = await fetch('http://localhost:3001/ingredients')
       const jsonData = await response.json()
-      console.log("jsonData: ", jsonData)
       setIngredients(jsonData);
     } catch (error) {
       console.error(error.message)
@@ -20,6 +19,7 @@ const Ingredient = () => {
     getIngredients();
   }, []);
 
+
   return ( 
       <section className="ingredients">
         <form>
@@ -27,54 +27,47 @@ const Ingredient = () => {
           <button>Search</button>
         </form>
         <div>
-          <h4>Vegetables & Greens ID1</h4>
-            <ul>
-              {ingredients.map(ingredient => {
+          <h4>Vegetables & Greens</h4>
+            {ingredients.map(ingredient => {
                 if (ingredient.category_id === 1) {
-                  return <li key={ingredient.id}>{ingredient.name}</li>;
+                  return <button key={ingredient.id}>{ingredient.name}</button>;
                 }
               })}
-            </ul>
-          <h4>Fruits ID2</h4>
-            <ul>
+              
+          <h4>Fruits</h4>
               {ingredients.map(ingredient => {
                 if (ingredient.category_id === 2) {
-                  return <li key={ingredient.id}>{ingredient.name}</li>;
+                  return <button key={ingredient.id}>{ingredient.name}</button>;
                 }
               })}
-            </ul>
-          <h4>Dairy & Eggs ID3</h4>
-            <ul>
+
+          <h4>Dairy & Eggs</h4>
               {ingredients.map(ingredient => {
                 if (ingredient.category_id === 3) {
-                  return <li key={ingredient.id}>{ingredient.name}</li>;
+                  return <button key={ingredient.id}>{ingredient.name}</button>;
                 }
               })}
-            </ul>
-          <h4>Dairy-Free & Meat Substitutes ID4</h4>
-            <ul>
+
+          <h4>Dairy-Free & Meat Substitutes</h4>
                 {ingredients.map(ingredient => {
                   if (ingredient.category_id === 4) {
-                    return <li key={ingredient.id}>{ingredient.name}</li>;
+                    return <button key={ingredient.id}>{ingredient.name}</button>;
                   }
                 })}
-              </ul>   
-          <h4>Meats & Poultry ID5</h4>
-            <ul>
+     
+          <h4>Meats & Poultry</h4>
               {ingredients.map(ingredient => {
                 if (ingredient.category_id === 5) {
-                  return <li key={ingredient.id}>{ingredient.name}</li>;
+                  return <button key={ingredient.id}>{ingredient.name}</button>;
                 }
               })}
-            </ul>
+
           <h4>Fish & Seafood ID6</h4>
-            <ul>
               {ingredients.map(ingredient => {
                 if (ingredient.category_id === 6) {
-                  return <li key={ingredient.id}>{ingredient.name}</li>;
+                  return <button key={ingredient.id}>{ingredient.name}</button>;
                 }
               })}
-            </ul>
         </div>
         
       </section>
