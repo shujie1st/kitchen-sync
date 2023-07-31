27b40f550fd92409935062ca1298c70f479772aa
Shujie Ma <shujie1st@gmail.com>
2023-07-30 21:49:57 -0400

    Add login form

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,7 +1,29 @@
+import { Button, Form } from "react-bootstrap";
+
 function Login(){
   return ( 
       <section className="login">
-        login component
+
+        <Form className="login-form">
+          <h2>Login</h2>
+
+          <Form.Group controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <br></br>
+            <Form.Control type="email" placeholder="Enter email" />
+          </Form.Group>
+
+          <Form.Group controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <br></br>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          
+          <Button type="submit">
+            Submit
+          </Button>
+        </Form>
+
       </section>
   );
 }
