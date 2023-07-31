b5f4f22e97d88eab606b06a2e0b4300fc247c6f6
Shujie Ma <shujie1st@gmail.com>
2023-07-30 20:35:18 -0400

    create react route for login page

--- client/src/App.js
+++ client/src/App.js
@@ -4,19 +4,28 @@ import Recipe from './components/Recipe';
 import Filter from './components/Filter';
 import './App.css';
 import Preference from './components/Preference';
+import Login from './components/Login';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
-      <Navigation />
-      <main className="container">
-        <Ingredient />
-        <div className="container-center">
-          <Preference />
-          <Recipe />
-        </div>
-        <Filter />
-      </main> 
+      <BrowserRouter>
+        <Navigation />
+        <Routes>
+          <Route path="/login" element={<Login />} />
+          <Route path="/" element={
+            <main className="container">
+              <Ingredient />
+              <div className="container-center">
+                <Preference />
+                <Recipe />
+              </div>
+              <Filter />
+            </main> 
+          } />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
--- client/src/components/Navigation.js
+++ client/src/components/Navigation.js
@@ -1,9 +1,12 @@
+import {Link} from "react-router-dom";
+
 function Navigation(){
   return ( 
       <nav>
         <span className="nav-text">KitchenSync</span>
 
         <div className="nav-right">
+          <Link to="/login">Login</Link>
         </div>
       </nav>
   );
