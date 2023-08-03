2d1dfb6b8d16ab7e0d3fb805ed1a8934cc08a69a
eliachow <eliacchow@gmail.com>
2023-08-03 16:02:24 -0700

    render preference list

--- client/src/components/UserPreferences.js
+++ client/src/components/UserPreferences.js
@@ -1,8 +1,39 @@
+import React, { useEffect, useState } from 'react';
+
 function UserPreferences() {
+
+  // database preferences
+  const [preferences, setPreferences] = useState([])
+
+  // fetch preferences from database
+  const fetchPreferences = async () => {
+    try {
+      const response = await fetch(`http://localhost:3001/preferences`)
+      const jsonData = await response.json()
+      setPreferences(jsonData)
+    } catch (error) {
+      console.error(error.message)
+    }
+  }
+
+  const getAllPreferences = () => {
+    return preferences.map(pref => {
+      return (
+      <button key={pref.id} >
+        {pref.name}
+      </button>
+      );
+    })
+  }
+
+  useEffect(() => {
+    fetchPreferences();
+  },[])
+
   return (
     <section className="user-preferences">
       UserPreferences component
-
+    {getAllPreferences()}
     </section>
   )
 
