128b4aec1e6d7bd4c383e805d727a64e5b78674b
Shujie Ma <shujie1st@gmail.com>
2023-08-07 18:12:55 -0400

    Add sign up form

--- client/src/components/Signup.js
+++ client/src/components/Signup.js
@@ -1,7 +1,47 @@
+import { Button, Card, Form } from 'react-bootstrap';
+
 function Signup() {
   return (
     <section className="signup">
-      Signup component
+       
+      <Card>
+        <Card.Body>
+          <h3>Sign Up</h3>
+            
+          <Form className='signup-from'>
+            
+            <Form.Group controlId="first-name">
+              <Form.Label>First Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter First Name" />
+            </Form.Group>
+
+            <Form.Group controlId="last-name">
+              <Form.Label>Last Name</Form.Label>
+              <Form.Control type="text" placeholder="Enter Last Name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicEmail">
+              <Form.Label>Email address</Form.Label>
+              <Form.Control type="email" placeholder="Enter email" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+              <Form.Label>Confirm Password</Form.Label>
+              <Form.Control type="password" placeholder="Password" />
+            </Form.Group>
+
+            <Button type="submit">
+              Create Account
+            </Button>
+          </Form>
+      
+        </Card.Body>
+      </Card>
     </section>
   )
 
